5058b65af06ec622ce04a449a3ff7a88a78b2450
septiningrumputri <xeotietie@gmail.com>
2023-06-05 10:46:14 +0700

    Initial commit

---
+++ app/src/main/java/com/septiningrum_202102248/recyclerviewa/MainActivity.java
@@ -0,0 +1,61 @@
+package com.septiningrum_202102248.recyclerviewa;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private RecyclerView _recyclerView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _recyclerView1 = (RecyclerView)findViewById(R.id.recycleView1);
+        List<NegaraModel> negaraModelList= new ArrayList<>();
+
+        NegaraModel nm = new NegaraModel();
+        nm.setNama("Albania");
+        nm.setDeskripsi("Negara ini menggunakan bendera berwarna merah.");
+        nm.setUrl("https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/48/Albania-Flag-icon.png");
+        negaraModelList.add(nm);
+
+        nm = new NegaraModel();
+        nm.setNama("Hungary");
+        nm.setDeskripsi("Negara ini bernama Hungaria dalam bahasa Indonesia.");
+        nm.setUrl("https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/48/Hungary-Flag-icon.png");
+        negaraModelList.add(nm);
+
+        nm = new NegaraModel();
+        nm.setNama("Slovenia");
+        nm.setDeskripsi("Belum ada deskripsi untuk negara ini.Silahkan ditambahkan bagi yang memiliki informasi .");
+        nm.setUrl("https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/48/Slovenia-Flag-icon.png");
+        negaraModelList.add(nm);
+
+        nm = new NegaraModel();
+        nm.setNama("Belgium");
+        nm.setDeskripsi("Namanya Belgia dalam Bahasa Indonesia. Negara ini terdapat di Eropa");
+        nm.setUrl("https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/48/Belgium-Flag-icon.png");
+        negaraModelList.add(nm);
+
+        nm = new NegaraModel();
+        nm.setNama("Canada");
+        nm.setDeskripsi("Negara ini terdapat disebelah utasa negara Amerika Serikat");
+        nm.setUrl("https://icons.iconarchive.com/icons/custom-icon-design/all-country-flag/48/Canada-Flag-icon.png");
+        negaraModelList.add(nm);
+
+        RecyclerView.LayoutManager lm =  new LinearLayoutManager(MainActivity.this);
+        _recyclerView1.setLayoutManager(lm);
+
+        NegaraAdapter na = new NegaraAdapter(getApplicationContext(), negaraModelList);
+        _recyclerView1.setAdapter(na);
+    }
+}
